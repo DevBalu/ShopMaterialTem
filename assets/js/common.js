b8d5e1f9890d36f2014e687674cefcd562999a7b
DevBalu <ghenov.andrei@gmail.com>
2016-11-09 13:14:15 +0200

    Add footer, menuTwo

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,9 +1,11 @@
 $(document).ready(function(){
+  /*SlideshowNav*/
 	$('.slider').slider({
 		full_width: true,
 		height: 800,
 		indicators: false
 	});
+  /*Navbar*/
 	$('.dropdown-button').dropdown({
       inDuration: 3,
       outDuration: 225,
@@ -14,5 +16,21 @@ $(document).ready(function(){
       alignment: 'right' // Displays dropdown with edge aligned to the left of button
     }
   );
+  /*Menu Two*/
+  $('.dropdown-button').dropdown2({
+      inDuration: 3,
+      outDuration: 225,
+      constrain_width: false, // Does not change width of dropdown to that of the activator
+      hover: true, // Activate on hover
+      gutter: 0, // Spacing from edge
+      belowOrigin: false, // Displays dropdown below the button
+      alignment: 'right' // Displays dropdown with edge aligned to the left of button
+    }
+  );
+   /*Visualization Categori Content*/
+  $('.collapsible').collapsible();
+  /**/
   $('.parallax').parallax();
+  /**/
+  $('.materialboxed').materialbox();
 });   
