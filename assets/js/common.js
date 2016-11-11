f3d861001e3343d9b9b46285d658808afa3f20bd
DevBalu <ghenov.andrei@gmail.com>
2016-11-11 11:52:10 +0200

    Integrate CollApsible

--- assets/js/common.js
+++ assets/js/common.js
@@ -16,17 +16,6 @@ $(document).ready(function(){
       alignment: 'right' // Displays dropdown with edge aligned to the left of button
     }
   );
-  /*Menu Two*/
-  $('.dropdown-button').dropdown2({
-      inDuration: 3,
-      outDuration: 225,
-      constrain_width: false, // Does not change width of dropdown to that of the activator
-      hover: true, // Activate on hover
-      gutter: 0, // Spacing from edge
-      belowOrigin: false, // Displays dropdown below the button
-      alignment: 'right' // Displays dropdown with edge aligned to the left of button
-    }
-  );
    /*Visualization Categori Content*/
   $('.collapsible').collapsible();
   /**/
