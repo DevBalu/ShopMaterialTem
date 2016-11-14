f9ea1fc0c1899c2cac01a0f1a21b7c981a21e708
DevBalu <ghenov.andrei@gmail.com>
2016-11-14 21:04:47 +0200

    Set brand menu, section_mini_categori, add section three - top product

--- assets/libs/flickity/flickityJS/flickity.pkgd.js
+++ assets/libs/flickity/flickityJS/flickity.pkgd.js
@@ -15,6 +15,31 @@
  * MIT license
  */
 
+ /*-----------------------------AG Custom----------------------*/
+ // external js: flickity.pkgd.js
+// external js: flickity.pkgd.js
+
+var carousel = document.querySelector('.carousel');
+var flkty = new Flickity( carousel, {
+  imagesLoaded: true,
+  percentPosition: false,
+});
+
+var imgs = carousel.querySelectorAll('.carousel-cell img');
+// get transform property
+var docStyle = document.documentElement.style;
+var transformProp = typeof docStyle.transform == 'string' ?
+  'transform' : 'WebkitTransform';
+
+flkty.on( 'scroll', function() {
+  flkty.slides.forEach( function( slide, i ) {
+    var img = imgs[i];
+    var x = ( slide.target + flkty.x ) * -1/8;
+    img.style[ transformProp ] = 'translateX(' + x  + 'px)';
+  });
+});
+/*------------------------------------END AG Cusom---------------*/
+
 /* jshint browser: true, strict: true, undef: true, unused: true */
 
 ( function( window, factory ) {
