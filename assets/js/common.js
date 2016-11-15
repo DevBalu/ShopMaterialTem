199a08724b0cc9ef837cf80ca45d6d63286d81b4
DevBalu <ghenov.andrei@gmail.com>
2016-11-15 17:54:30 +0200

    Global changes: 1-set dropdown menu,common.js auto attrib id . 2-Set mini categories. 3 -brands slideshow

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,12 +1,23 @@
+/*Set attribute dropdown*/
+(function() {
+  var drop = $(".drop");
+  var dropactive = $(".dropactive");
+
+  for (var i = 0; i < drop.length; i++) {
+    $(drop[i]).attr("id", "dropdown" + i);
+    $(dropactive[i]).attr("data-activates", "dropdown" + i);
+  }
+})();
+
 $(document).ready(function(){
   /*SlideshowNav*/
-	$('.slider').slider({
-		full_width: true,
-		height: 800,
-		indicators: false
-	});
+  $('.slider').slider({
+    full_width: true,
+    height: 800,
+    indicators: false
+  });
   /*Navbar*/
-	$('.dropdown-button').dropdown({
+  $('.dropdown-button').dropdown({
       inDuration: 3,
       outDuration: 225,
       constrain_width: false, // Does not change width of dropdown to that of the activator
@@ -16,10 +27,16 @@ $(document).ready(function(){
       alignment: 'right' // Displays dropdown with edge aligned to the left of button
     }
   );
-   /*Visualization Categori Content*/
-  $('.collapsible').collapsible();
+   /*Visualization Categori Content*/  
+  $('.collapsible').collapsible({
+      accordion: false, // A setting that changes the collapsible behavior to expandable instead of the default accordion style
+      onOpen: function(el) { alert('Open'); }, // Callback for Collapsible open
+      onClose: function(el) { alert('Closed'); } // Callback for Collapsible close
+    }
+  );
   /**/
   $('.parallax').parallax();
   /**/
   $('.materialboxed').materialbox();
+  
 });   
